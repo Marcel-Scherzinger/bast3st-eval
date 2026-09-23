55b156bb7b16a7b0792eff9df197ca04f81d541d
Marcel Scherzinger <>
2026-09-23 16:33:37 +0200

    eval: restructure features

--- src/evaluation/features.rs
+++ src/evaluation/features.rs
@@ -13,27 +13,28 @@ bitflags::bitflags! {
     impl Features : u32 {
         /// Allow access to INPUT, OUTPUT, RANDOMS, LISTS, ...
         const READ_RUNDATA = 1 << 0;
+        /// End a current test (available in test hooks and tests)
         const END_THIS_TEST = 1 << 1;
+        /// Send global message (available if message sending is allowed)
         const SENDMSG_SPEC = 1 << 2;
+        /// Send message to category (available in category and its tests)
         const SENDMSG_CATEGORY = 1 << 3;
+        /// Send message to the maintest level (only in main- and alternative test)
         const SENDMSG_MAINTEST = 1 << 4;
-        const SENDMSG_THISTEST = 1 << 5; // TODO: replace with current?
+        /// Send message to the current entity level
+        const SENDMSG_CURRENT = 1 << 5;
 
-        const PermittedFEAT_TestScope =
-              Self::SENDMSG_SPEC.0
-            | Self::SENDMSG_CATEGORY.0
-            | Self::SENDMSG_MAINTEST.0
-            | Self::SENDMSG_THISTEST.0
-            | Self::READ_RUNDATA.0;
-        const PermittedFEAT_PassTestCrit =
-              Self::PermittedFEAT_TestScope.0
-            | Self::END_THIS_TEST.0;
         const PermittedFEAT_PreTestHook =
               Self::END_THIS_TEST.0
             | Self::SENDMSG_SPEC.0
             | Self::SENDMSG_CATEGORY.0
             | Self::SENDMSG_MAINTEST.0
-            | Self::SENDMSG_THISTEST.0;
+            | Self::SENDMSG_CURRENT.0;
+        const PermittedFEAT_TestScope =
+              Self::PermittedFEAT_PreTestHook.0
+            | Self::READ_RUNDATA.0;
+        const PermittedFEAT_PassTestCrit = Self::PermittedFEAT_TestScope.0;
+        const PermittedFEAT_PostTestHook = Self::PermittedFEAT_TestScope.0;
     }
 }
 
