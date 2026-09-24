ea364f050ac0d5f7fdc4a66d3794f3ed13ebc126
Marcel Scherzinger <>
2026-09-24 09:10:15 +0200

    eval: enhance hook processing

--- src/evaluation/process_hook.rs
+++ src/evaluation/process_hook.rs
@@ -15,7 +15,7 @@ use crate::{
     },
 };
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, thiserror::Error, PartialEq, PartialOrd, Clone)]
 pub enum HookFailure {
     #[error("criterion: {_0}")]
     Criterion(SingleEvaluationError),
@@ -23,9 +23,10 @@ pub enum HookFailure {
     Action(SingleEvaluationError),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
 pub struct HookResult {
     pub(crate) actions: Vec<RuntimeAction>,
+    pub(crate) eval_signals: Option<StopEval>,
 }
 impl HookResult {
     pub async fn from_pre_hook<Source: SelectableSource>(
@@ -63,50 +64,62 @@ impl HookResult {
     }
 }
 
-/// (Adds [`Features::END_THIS_TEST`] to given base features)
 pub(crate) async fn execute_hook<'p, 'e, Source: SelectableSource>(
     settings: &Context<'p, 'e>,
     outer_fallback: Source,
-    base_features: Features,
+    features: Features,
     crit: EntityId<CriterionEntity>,
     act: EntityId<ActionEntity>,
 ) -> Result<HookResult, HookFailure> {
-    let base_features = base_features | Features::END_THIS_TEST;
     let mut actions = vec![];
 
-    let c_eval = SingleEvaluation::new(
+    let crit_eval = SingleEvaluation::new(
         settings.entities(),
         crit.cast_id(),
         &outer_fallback,
-        base_features,
+        features,
     )
     .map_err(SingleEvaluationError::from)
     .map_err(HookFailure::Criterion)?;
-    let c_eval = c_eval.run_to_end_with_early_return().await;
+    let crit_eval = crit_eval.run_to_end_with_early_return().await;
 
-    let criterion: Either<StopEval, RuntimeCriterion> =
-        c_eval.one_specialized().map_err(HookFailure::Criterion)?;
+    let criterion: Either<StopEval, RuntimeCriterion> = crit_eval
+        .one_specialized()
+        .map_err(HookFailure::Criterion)?;
 
-    if criterion.is_right_and(|criterion| criterion.is_fulfilled()) {
-        let a_eval = SingleEvaluation::new(
-            settings.entities(),
-            act.cast_id(),
-            &outer_fallback,
-            base_features,
-        )
-        .map_err(SingleEvaluationError::from)
-        .map_err(HookFailure::Action)?;
-        let a_eval = a_eval.run_to_end_with_early_return().await;
+    let eval_signals = match criterion.map_left(Some) {
+        Either::Left(eval_signals) => eval_signals,
+        Either::Right(criterion) => {
+            if criterion.is_fulfilled() {
+                let act_eval = SingleEvaluation::new(
+                    settings.entities(),
+                    act.cast_id(),
+                    &outer_fallback,
+                    features,
+                )
+                .map_err(SingleEvaluationError::from)
+                .map_err(HookFailure::Action)?;
+                let act_eval = act_eval.run_to_end_with_early_return().await;
 
-        let action: Either<StopEval, RuntimeAction> =
-            a_eval.one_specialized().map_err(HookFailure::Criterion)?;
+                let action: Either<StopEval, RuntimeAction> =
+                    act_eval.one_specialized().map_err(HookFailure::Criterion)?;
 
-        actions.extend(c_eval.into_actions());
-        actions.extend(a_eval.into_actions());
-        if let Either::Right(action) = action {
-            actions.push(action);
+                actions.extend(crit_eval.into_actions());
+                actions.extend(act_eval.into_actions());
+                match action.map_left(Some) {
+                    Either::Left(eval_signals) => eval_signals,
+                    Either::Right(action) => {
+                        actions.push(action);
+                        None
+                    }
+                }
+            } else {
+                None
+            }
         }
-    }
-
-    Ok(HookResult { actions })
+    };
+    Ok(HookResult {
+        actions,
+        eval_signals,
+    })
 }
