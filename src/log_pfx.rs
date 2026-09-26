442d15a307ea24a862db9a9a3a09e2db292e168f
Marcel Scherzinger <>
2026-09-26 14:08:47 +0200

    main: make LogPfx storage optional

--- src/log_pfx.rs
+++ src/log_pfx.rs
@@ -1,19 +1,40 @@
-use std::fmt::Debug;
-
-use derive_more::{Deref, Display, From};
-
-#[derive(derive_more::Debug, Display, Deref, Clone, PartialEq, Eq, PartialOrd, Ord, From)]
+#[cfg(not(feature = "no-log-pfx"))]
+#[derive(derive_more::Debug, derive_more::Display, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct LogPfx(Box<str>);
+
+#[cfg(not(feature = "no-log-pfx"))]
 impl LogPfx {
     pub fn new(t: impl Into<Box<str>>) -> Self {
         Self(format!("{:?}", t.into()).into())
     }
-    pub fn join(&self, text: impl Debug) -> LogPfx {
+    pub fn join(&self, text: impl std::fmt::Debug) -> LogPfx {
         LogPfx(format!("{}/{:?}", self.0, text).into())
     }
 }
+#[cfg(not(feature = "no-log-pfx"))]
 impl From<&str> for LogPfx {
     fn from(value: &str) -> Self {
         Self(format!("{value:?}").into())
+    }
+}
+
+#[cfg(feature = "no-log-pfx")]
+#[derive(derive_more::Debug, derive_more::Display, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[display("")]
+pub struct LogPfx(());
+
+#[cfg(feature = "no-log-pfx")]
+impl LogPfx {
+    pub fn new(t: impl Into<Box<str>>) -> Self {
+        Self(())
+    }
+    pub fn join(&self, text: impl std::fmt::Debug) -> LogPfx {
+        LogPfx(())
+    }
+}
+#[cfg(feature = "no-log-pfx")]
+impl From<&str> for LogPfx {
+    fn from(value: &str) -> Self {
+        Self(())
     }
 }
